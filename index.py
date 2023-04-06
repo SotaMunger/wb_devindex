8670a80af3350f8f79c198669a3d331e63cc94ec
Richard Bridges <lee.bridges23@ncf.edu>
2023-04-05 22:54:13 -0400

    (feat)Added page3.py (GDP) and updated index.py to link to page3

--- index.py
+++ index.py
@@ -6,7 +6,7 @@ from dash.dependencies import Input, Output
 from app import app, server
 
 # Connect to your app pages
-from pages import home, page1b, page2b
+from pages import home, page1b, page2b, page3
 
 # Connect the navbar to the index
 from components import navbar
@@ -22,10 +22,12 @@ nav = dbc.NavbarSimple(
             children = [
                 dbc.DropdownMenuItem("Home", href = "http://127.0.0.1:8050/home"),
                 dbc.DropdownMenuItem("Inflation", href = "http://127.0.0.1:8050/inflation"),
-                dbc.DropdownMenuItem("Imports", href = "http://127.0.0.1:8050/imports")
+                dbc.DropdownMenuItem("Imports", href = "http://127.0.0.1:8050/imports"),
+                dbc.DropdownMenuItem("Growth", href = "http://127.0.0.1:8050/growth")
                 # dbc.DropdownMenuItem("Home", href = "http://wb-devindex.onrender.com"),
                 # dbc.DropdownMenuItem("Inflation", href = "http://wb-devindex.onrender.com/inflation"),
-                # dbc.DropdownMenuItem("Imports", href = "http://wb-devindex.onrender.com/imports")
+                # dbc.DropdownMenuItem("Imports", href = "http://wb-devindex.onrender.com/imports"),
+                # dbc.DropdownMenuItem("Growth", href = "http://wb-devindex.onrender.com/growth")
             ],
             nav=True,
             in_navbar = True,
@@ -52,6 +54,8 @@ def display_page(pathname):
         return page1b.layout
     if pathname == '/imports':
         return page2b.layout
+    if pathname == '/growth':
+        return page3.layout
     else:
         return home.layout
 
